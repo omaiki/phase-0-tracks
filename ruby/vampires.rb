2b886517bbb970c042ac713d5965b9a2c9da8e34
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-08 23:08:50 -0700

    Questions and user input for release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.to_i
+puts "What year were you born?"
+year = gets.to_i
+puts "Our company cafeteria has world famous garlic break, should we order some for you during company lunch?"
+garlic = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+health = gets.chomp
