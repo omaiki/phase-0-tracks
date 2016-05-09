0fd7c4a66294eb1180219e38fab2e857097e0ee3
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-09 00:31:46 -0700

    Added first if statement for release 2, working

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,7 +4,17 @@ puts "How old are you?"
 age = gets.to_i
 puts "What year were you born?"
 year = gets.to_i
-puts "Our company cafeteria has world famous garlic break, should we order some for you during company lunch?"
+puts "Our company cafeteria has world famous garlic break, should we order some for you during company lunch? (yes/no)"
 garlic = gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (yes/no)"
 health = gets.chomp
+
+if (age == 2016 - year) && ((garlic == "yes") || (health == "yes"))
+  result = "Probably not a vampire."
+  p result
+elsif
+
+
+end
+
+
