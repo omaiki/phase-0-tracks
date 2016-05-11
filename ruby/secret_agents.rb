c27916ac0c298cc2bbec979c1fe35de03d5f84fd
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-10 23:05:40 -0700

    Pseudocode for 4.6 decrypt and encrypt, draft

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,15 @@
+Pseudocode: 
+
+		- Encrypt: 
+			- Define method with name
+			- Remove first letter in string at index 0
+			- Advance one letter forward from last index on string
+
+
+
+
+
+		- Decrypt: 
+			- Define method with name
+			- Shift string one letter back from last index using most negative index
+			- Remove least negative (most positive) index (last letter in string)
