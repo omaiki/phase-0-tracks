feb10c9d71b35856c8855068d057cfa9a9a722fe
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-15 17:53:49 -0700

    Finishing up user input for release 3, all working

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -5,10 +5,10 @@
 # Get user input through puts statements and get.chomp, store into variable 
 	# name -> string
 	# age -> integer 
+	# team -> string
 	# seasons in NBA -> integer
 	# height -> string  
-	# team -> string
-	# Championship? -> boolean
+	# Championship? -> boolean -> string
 	# Field goal % -> float
 	# Free throw % -> float
 	# 3 point # -> float 
@@ -27,8 +27,25 @@ age = gets.chomp
 player_info[:age] = "#{age}".to_i
 p player_info
 
+puts "What team is the player on?"
+team = gets.chomp
+player_info[:team] = "{team}"
+
+puts "How many seasons has player been in the NBA"
+seasons = gets.chomp
+player_info[:seasons] = "#{seasons}".to_i
+
 puts "Player height?"
 height = gets.chomp 
 player_info[:height] = "#{height}"
+
+puts "(true/false) The player has won a championship"
+champion = gets.chomp
+player_info[:champion] = "#{champion}"
+
+puts "Player Field Goal Percentage?"
+fg_percent = gets.chomp
+player_info[:fg_percent] = "#{fg_percent}".to_f
 p player_info
+
 
