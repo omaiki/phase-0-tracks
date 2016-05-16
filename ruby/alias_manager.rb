41d021521be2e9823ca16bbbb01ffbbed783fd28
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-16 02:41:38 -0700

    Pseudocode finished for 5.6

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,14 @@
+# Pseudocode:
+
+# define method
+# get user input
+# convert user input to downcase
+# seperate first and last name, than swap
+# split words into characters
+	# if character is a vowel
+		# character should go to next vowel
+	# else
+		# character should go to next consonant
+	#end
+# convert fake name to capitalized fake name 
+
