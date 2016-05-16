f3bc021468ac7cb7b3778f22551bada6801121bf
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-15 23:31:46 -0700

    Finished nested data structured, working on printing diff types of access

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,39 @@
+team_management = {
+# hash of all the suits in warriors organization
+    the_suits: {
+# Hash of executive board 
+        executive_board: {
+            ceo: 'Joe Lacob',
+            co_executive_chairman: "Peter Guber"
+# Array of executive board members
+            exec_board_member: [
+            "Jerry West"
+            "Mark Stevens"
+            "Bruce Karsh"
+            "Jeff Miller"
+            "Chamath Palihapitiya"
+            ]
+            },
+# Hash of basketball ops people in warriors organization
+        basketball_ops: {
+            gen_manager: "Bob Myers"
+            asst_gm: [
+                "Travis Schlenk"
+                "Kirk Lacob"
+            ]
+            director_player_personal: "Larry Harris"
+            head_coach: "Steve Kerr"
+            asst_coach: [ 
+                "Luke Walton"
+                "Ron Adams"
+                "Jarron Collins"
+                "Steve Nash"
+            ]
+            player_development: [
+                "Bruce Fraser"
+                "Chris DeMarco"
+            ]
+
+        }
+    }
+}
