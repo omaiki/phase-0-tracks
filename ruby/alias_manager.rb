114caaf4424c5eef0e985097e6f2845a8770e7a7
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-16 03:05:23 -0700

    Have user input for name swapped in an array, working, 5.6

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,7 +3,19 @@
 # define method
 # get user input
 # convert user input to downcase
-# seperate first and last name, than swap
+
+def alias_method
+	puts "What is your name, agent?"
+	name = gets.chomp.downcase
+# seperate first and last name and save array as variable
+	split_name = name.split(' ')
+# than swap using shuffle method, use bang op to change existing array
+	split_name.shuffle!
+	p split_name
+end
+
+
+
 # split words into characters
 	# if character is a vowel
 		# character should go to next vowel
@@ -11,4 +23,8 @@
 		# character should go to next consonant
 	#end
 # convert fake name to capitalized fake name 
+
+
+p alias_method
+
 
