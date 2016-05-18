8c92596b04a186102095cc3ca2532add63bb7a0e
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-18 00:01:18 -0700

    Finished Release 0, design a class for puppy, 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,35 @@
+
+=begin 
+
+CLASS ---------------------
+  
+  	Puppy 
+
+CHARACTERISTICS ------------
+	
+	Name: varies
+	Fur color: varies
+	Noise : Bark  
+	Legs: 4
+
+
+
+
+BEHAVIOR -------------------
+	
+	Fetch
+	Lick
+	Jump
+	Tail wag
+	
+
+
+
+
+
+
+
+
+
+
+=end 
