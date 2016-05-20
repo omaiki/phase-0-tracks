6e5024317eb6f23a343e2ee6300e752a1f7c0803
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-19 23:27:57 -0700

    Speak method and fetch method working for puppy instance

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,9 +5,24 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+  	integer.times do |integer|
+  		puts "Woof!"
+  	end
+  	end
+  		
+
+  def roll_over
+  end
+
+  def dog_years
+  end
+
 end
 
 
 
 buddy = Puppy.new
 buddy.fetch("stuffed animal")
+
+buddy.speak(7)
