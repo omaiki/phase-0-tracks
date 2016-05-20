a83389adfc5c81b61b5229122cfaac39335cb0d4
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-19 23:46:05 -0700

    All methods working for puppy instance, 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -13,16 +13,31 @@ class Puppy
   		
 
   def roll_over
+  	p "*rolls over*"
   end
 
-  def dog_years
+  def dog_years(human_years)
+  	dog_years = human_years / 6.5
+  	# dog_years is a float, so round out normally
+  	p dog_years.round
   end
 
 end
 
 
-
+#initalizing instance (buddy) of Puppy class
 buddy = Puppy.new
+#calling fetch method on instance buddy
 buddy.fetch("stuffed animal")
-
+#calling speak method on instance buddy
 buddy.speak(7)
+#calling roll_over method on instance buddy
+buddy.roll_over
+
+buddy.dog_years(50)
+
+#any other instance of Puppy should be able to call these methods as
+# long as it is initalized
+
+snoopy = Puppy.new
+snoopy.fetch("jerky")
