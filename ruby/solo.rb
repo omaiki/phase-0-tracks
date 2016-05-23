da3fc10c350feb343c032eb0184e4b4f4475f7bb
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-23 00:03:35 -0700

    Initiation code and one method working for solo challenge

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,23 +1,25 @@
 =begin
 PSEUDOCODE FOR SOLO CHALLENGE: 3 attributes (2 data types) and three methods (one argument at least)
 
-Class Player
-defaults: "No set position", 0, "N/A", "No specialty", false
+Class Player-----------
 
-Attributes:
+defaults:
+
+Attributes-------------
+
+Name: (string)
 Height: (string)
 Jersey #: (integer)
-Shooting: (integer)
-Dribbling: (integer)
-Rebounding: (integer)
+Specialty Skills: (array of shooting, dribbling, passing, rebounding, defense)
 
-Methods:
 
-  Training method (string, integer):
-    IF attribute (shooting, dribbling, rebounding) integer is below 50,
-      -  player will go into training for that skill
-    ELSE
-      - player will skip training and scrimmage instead
+Methods------------------
+
+  initalize method(name, height, jersey_num)
+
+
+  Assign a skill method (array)
+   - assigns a skill to your player form the array skills
 
   Position method (string argument):
     IF attribute (height) is less than 6'7
@@ -27,8 +29,28 @@ Methods:
     ELSE
     - player position will be center
 
-  Request a trade method (string, integer):
-    Change jersey # and team attributes to new_jersey # and new_team attributes
-
-
+  Shoot method(player_name)
+    print out player shooting ball and making a basket...
+    ----------------------------------------------------
 =end
+
+class Player_bball
+
+  def initialize(player_name, height, jersey)
+    p "Creating #{player_name}..."
+    p "#{height}"
+    p "#{jersey}"
+    @height = height
+    @jersey = jersey
+    @skill_choices = ["shooting", "dribbling", "passing", "rebounding", "defense"]
+  end
+
+  def assign
+   p @skill_choices.sample(2)
+  end
+
+end
+
+player_one = Player_bball.new("Oshoke", "5'10", 21)
+player_one.assign
+
