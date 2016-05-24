061839dece00649e27535b366395370ac55d4cf0
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-24 16:05:39 -0700

    Created first JS file, practicing loops and methods

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+// Function takes a string as a paramater and reverses string
+
+  // declare Function with string parameter
+  // For each string:
+      // split into an array
+      // reverse order of array
+      // return reversed-array
+      // join back into a string
+      // print reversed string
+
+
+function reverse_string(string) {
+   for (var i = )
+}
+
