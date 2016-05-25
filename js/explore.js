c1004eceb7572573656cc312421e6822d2f70872
Oshoke Maiki <oshokemaiki@gmail.com>
2016-05-24 17:03:12 -0700

    Reverse string function working and tested

--- js/explore.js
+++ js/explore.js
@@ -4,12 +4,23 @@
   // For each string:
       // split into an array
       // reverse order of array
-      // return reversed-array
       // join back into a string
       // print reversed string
 
-
-function reverse_string(string) {
-   for (var i = )
+function reverseString(string) {
+  var newString = string.split('').reverse().join('');
+  // testing to see if code is working within function by printing
+  console.log(newString);
+  // store value for when function is called, variable not necessarily needed, can put 'return' on line 11 instead of variable
+  return newString;
 }
+
+
+var reverseHello = reverseString("hello");
+console.log(reverseHello);
+console.log((reverseHello) + "Oshoke");
+
+
+
+
 
