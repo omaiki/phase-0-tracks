2f8266631c1a99e41169b8288e0f2c8ec3807370
Oshoke Maiki <oshokemaiki@gmail.com>
2016-06-05 21:47:56 -0700

    Finished Release 2, working function

--- js/algos.js
+++ js/algos.js
@@ -30,7 +30,32 @@ for(key in x) {
 console.log(objectMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
 console.log(objectMatch({person: "Oshoke", age:7}, {person: "Emily", age:10}));
 
+// Release 2
 
+function generateArray(integer) {
+  var letters = 'abcdefghijklmnopqrstuvwxyz';
+  var wordsArray = [];
+  for(var i = 0; i < integer; i++) {
+    var word = "";
+    var wordLength = Math.floor(Math.random() * 10 + 1);
+    for(var j = 0; j < wordLength; j++) {
+      word = word + letters[j]
+    }
+    wordsArray.push(word);
+  }
+  console.log(wordsArray)
+  return wordsArray;
+}
+
+
+console.log(longestPhrase(generateArray(10)))
+
+// for(var i = 0; i < 10; i++) {
+//   var amount = Math.floor(Math.random() * 5 + 2);
+//   var list = generateTestData(amount);
+//   console.log(list);
+//   console.log(returnLongest(list));
+// }
 
 
 
