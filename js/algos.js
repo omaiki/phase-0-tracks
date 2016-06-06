22504e19a10011573cfd9ba77e18d9c8a4de38f6
Oshoke Maiki <oshokemaiki@gmail.com>
2016-06-05 21:33:16 -0700

    Finished release 1, objectMatch function working

--- js/algos.js
+++ js/algos.js
@@ -1,13 +1,39 @@
-//Release 0
+// RELEASE 0 - LONGEST PHRASE
 
 function longestPhrase(input) {
-  var longest = "";
-for(var i = 0; i < input.length, i++) {
-    if (input[i].length > longest.length) {
-        longest = input[i];
+  var longest_phrase = "";
+for(var i = 0; i < input.length; i++) {
+    if (input[i].length > longest_phrase.length) {
+        longest_phrase = input[i];
     }
   }
-return longest;
+return longest_phrase;
 }
 console.log(longestPhrase(["long phrase","longest phrase","longer phrase"]));
+
+// Release 1
+
+function objectMatch(x,y) {
+for(key in x) {
+  var matchKey = key;
+  var matchValue = x[key];
+  for(key in y) {
+    if(matchKey == key &&
+      matchValue == y[key]) {
+      return true;
+    }
+  }
+}
+  return false;
+}
+
+console.log(objectMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+console.log(objectMatch({person: "Oshoke", age:7}, {person: "Emily", age:10}));
+
+
+
+
+
+
+
 
