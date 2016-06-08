e021248973f14a09d86195ab3a8768b6ed2acae0
Oshoke Maiki <oshokemaiki@gmail.com>
2016-06-08 15:17:56 -0700

    Able to add name and weight to database from interface, working

--- databases/sql_and_ruby/gym_project.rb
+++ databases/sql_and_ruby/gym_project.rb
@@ -15,65 +15,96 @@ db = SQLite3::Database.new("gym_project.db")
 db.results_as_hash = true
 
 
+
+
+# method that obtains exercise choice and 1RM of user, does Calculation method for next workout based on desired intensity
+def progressive_overload
+  puts "Exercise choice?"
+  exercise = gets.chomp
+  puts "ONE REPETITION MAXIMUM for exercise/lift?"
+  one_RM = gets.chomp.to_i
+  puts "Intensity? (1-3; 3 being highest)"
+  intensity = gets.chomp
+    case intensity
+    when "1"
+      progression_calc = one_RM * 0.75
+      progression = "Complete three sets, 10 repetitions of #{progression_calc.to_i.to_s} pounds at your next workout."
+    when "2"
+        progression_calc = one_RM * 0.80
+        progression = "Complete three sets, 8 repetitions of #{progression_calc_to_i.to_s} pounds at your next workout."
+    when "3"
+        progression_calc = one_RM * 0.90
+        progression = "Complete three sets, 4 repetitions of #{progression_calc.to_i.to_s} pounds at your next workout."
+    end
+    p exercise
+    p progression
+end
+progressive_overload
+
+
 #declaring a string for creating a table
 create_table_cmd = <<-SQL
 CREATE TABLE IF NOT EXISTS user(
   id INTEGER PRIMARY KEY,
   name VARCHAR(255),
-  age INTEGER,
+  weight INTEGER,
   exercise VARCHAR(255),
   progression VARCHAR(255)
 )
 SQL
+# #executing the string, creating a table
 
-# method to add workout info(exercise and progression) into database
+db.execute(create_table_cmd)
+
+# # method to add workout info(exercise and progression) into database
 def workout_info(db, exercise, progression)
   db.execute("INSERT INTO user (exercise, progression) VALUES (?, ?)", [exercise, progression])
 end
 
-#INTERFACE
-
-# obtain workout and 1RM of user, do Calculation method for 1RM and next time workout
-def progressive_overload
-  puts "Enter your name:"
-  name = gets.chomp
-  puts "Enter your age:"
-  age = gets.chomp
-  puts "Exercise choice?"
-  exercise = gets.chomp
-  puts "ONE REPETITION MAXIMUM for exercise/lift?"
-  one_RM = gets.chomp.to_i
-  progression_calc = one_RM * 0.75
-  progression = "Complete three sets, 10 repetitions of #{progression_calc.to_i.to_s} pounds at your next workout."
-  p progression
-end
-
-# add some if/else or case statements for gender/age down the line
-
-#executing the string, creating a table
-answer = ''
-puts "Hi there! Welcome to the Progressive Overload Simulator. Type 'done' if you are finished."
-until answer = 'done'
-db.execute(create_table_cmd)
-progressive_overload
-end
-
-#Added a user with stored info to (user) table through a method with input as arguments
-    #db.execute("INSERT INTO user(name, weight, workout) VALUES ('Joseph', 170, 'squat')")
-    # need the database as an argument
-    # use ? placeholder for user input data instead of interpolation --- security
-
+#method to create user on user table
 def create_user(db, name, weight)
   db.execute("INSERT INTO user (name, weight) VALUES (?, ?)", [name, weight])
 end
 
-#making names in Faker
 
-# 3.times do
-#   create_user(db, Faker::Name.name, 170,"benchpress")
+
+
+
+
+#INTERFACE
+
+answer = ''
+puts "Hi there! Welcome to the Progressive Overload Simulator...."
+until answer == 'exit'
+  puts "Enter your name: "
+  name = gets.chomp
+  puts "Enter your weight: "
+  weight = gets.chomp
+  #making a user
+  create_user(db, name, weight)
+puts "Press enter to continue with another user or ,'exit', if you are finished!"
+answer = gets.chomp
+end
+
+#progressive_overload
+
+
+
+
+
+
+# def create_user(db, name, weight)
+#   db.execute("INSERT INTO user (name, weight) VALUES (?, ?)", [name, weight])
 # end
+
+
 
 # user = db.execute("select * from user")
 # user.each do |user|
 #   puts "#{user['name']} weighs #{user['weight']} pounds. Workout of choice is: #{user['workout']}."
 # end
+
+
+#take users name, weight, workout
+# method that takes user 1RM max in a workout and calculates exercises for next workout
+# put name, weight, workout, next_workout into a database to show multiple exercises for a user.
