5b794de553f048d74b426b0ca8f7d261431d9be4
Oshoke Maiki <oshokemaiki@gmail.com>
2016-06-11 23:08:15 -0700

    Function working with link to js file

--- web_dev/client_side_js/practice.js
+++ web_dev/client_side_js/practice.js
@@ -1,16 +1,28 @@
-console.log("link to js file is working!");
+console.log("The link to js file is working!");
 
 //event listener on photo to be clicked
   // need function
 
-function addBorder() {
-  var photo = document.getElementbyId("lizard-photo");
-  photo.style.border= "2 px solid blue";
+// function addBorder(event) {
+//   event.target.style.border = "3 px solid blue";
+//   }
+
+function cursiveThat() {
+  var text = document.getElementById('about-my-pet');
+  text.style.color= "blue";
+};
+
+cursiveThat();
+
+function increaseSize() {
+  var textDescription =
 }
 
-// grab photo and put event-listener on the element
-var photo = document.getElementbyId("lizard-photo");
+// // grab photo and put event-listener on the element
 
-// addevent function can be used on an element
-//arguments = (what type of event as a string, then give it code you want to run when it type of event is triggered)
-photo.addEventListener("click", addBorder);
+// var photo = document.getElementbyId("lizard-photo");
+
+// // addevent function can be used on an element
+// //arguments = (what type of event as a string, then give it code you want to run when it type of event is triggered)
+
+// photo.addEventListener("click", addBorder);
